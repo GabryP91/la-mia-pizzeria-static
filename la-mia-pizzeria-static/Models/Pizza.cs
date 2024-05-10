cb1e86782c50ccd7a011e2614f61999bc86e3bd3
Gabriele <gabriele.palma.bool@gmail.com>
2024-05-10 16:30:05 +0200

    add

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace la_mia_pizzeria_static.Models
+{
+    [Table("Pizza")]
+    [Index(nameof(Nome), IsUnique = true)]
+    public class Pizza
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string Foto { get; set; }
+        public decimal Prezzo { get; set; }
+
+        public Pizza() {}
+
+        public Pizza(string nome, string descrizione, string foto, decimal prezzo) 
+        {
+            this.Nome = nome;
+
+            this.Descrizione = descrizione;
+
+            this.Foto = foto;
+
+            this.Prezzo = prezzo;
+
+        }
+
+        //override del metodo ToString
+        public override string ToString()
+        {
+            return $"\nNome: {Nome} - Descrizione: {Descrizione} - Foto: {Foto} - Prezzo: {Prezzo}";
+        }
+    }
+}
